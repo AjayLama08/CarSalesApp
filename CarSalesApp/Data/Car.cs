10fb1c14716d92bacb25bcefe6d3b50c4c7b7bfe
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-08 13:47:48 +0100

    Updated

--- CarSalesApp/Data/Car.cs
+++ CarSalesApp/Data/Car.cs
@@ -6,6 +6,41 @@ namespace CarSalesApp.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public Fuel Fuel { get; set; }
+        public double Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool Sold { get; set; }
+
+        //Default COnstructor
+        public Car()
+        {
+            Name = string.Empty;
+            Manufacturer = string.Empty;
+        }
+
+        public Car(int id, string name, string manufacturer, int year, Fuel fuel, double price, DateTime dateAdded, bool sold)
+        {
+            Id = id;
+            Name = name;
+            Manufacturer = manufacturer;
+            Year = year;
+            Fuel = fuel;
+            Price = price;
+            DateAdded = dateAdded;
+            Sold = sold;
+        }
+
+        public override string ToString()
+        {
+            var str = Id + ": " + Manufacturer + "-" + Name + "(" + Year + ") £" + Price + " " + Fuel + "(" + DateAdded.ToShortDateString() + ")";
+            if (Sold)
+            {
+                str = str + "**";
+            };
+            return str;
+        }
 
     }
 }
