6e013feef38321e3f0faeac5f4d6d4239f9402d1
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-08 14:33:17 +0100

    Add code to update database record 7

--- CarSalesApp/Program.cs
+++ CarSalesApp/Program.cs
@@ -10,13 +10,25 @@ static void displayCars(List<Car> cars)
 // Connect and open database
 using var db = new CarSalesDbContext();
 
-// Load all car records from database
-var cars = db.Cars.ToList();
+//// Load all car records from database
+//var cars = db.Cars.ToList();
 
-// Display all car records from memory list
-displayCars(cars);
+//// Display all car records from memory list
+//displayCars(cars);
 
-// Create a new car record
-var newCar = new Car(0, "Corolla", "Toyota", 2023, Fuel.HYBRID, 13995, DateTime.Now, false);
-db.Cars.Add(newCar);
+//// Create a new car record
+//var newCar = new Car(0, "Corolla", "Toyota", 2023, Fuel.HYBRID, 13995, DateTime.Now, false);
+//db.Cars.Add(newCar);
+//db.SaveChanges();
+
+// load in car with id 7
+var car = db.Cars
+.Where(c => c.Id == 7)
+.SingleOrDefault(); // get a single record or NULL
+
+Console.WriteLine($"Before change: {car}");
+car.Price = 19500;
+Console.WriteLine($"After change: {car}");
+
+// Save change to memory objects to the database
 db.SaveChanges();
