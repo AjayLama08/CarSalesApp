e916ebf7eefb1f922929d8727b979d44165e8e89
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-08 13:23:35 +0100

    Updated

---
+++ CarSalesApp/Data/Car.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSalesApp.Data
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
